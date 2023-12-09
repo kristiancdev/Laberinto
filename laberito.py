f6f9207f4664216bfb0e5f06945a593afa544dad
Cristian Cevallos <81939696+kristiancdev@users.noreply.github.com>
2023-12-09 04:54:08 -0500

    Update laberito.py
    Se implementa la navegación pero aún no consigo el objetivo para que el cursor se mueva dentro del laberinto

--- laberito.py
+++ laberito.py
@@ -14,6 +14,15 @@ ventana.bgcolor("white")
 laberinto_turtle = turtle.Turtle()
 laberinto_turtle.speed(0)  # Configurar la velocidad del dibujo
 
+# Posición inicial de la tortuga
+inicio_x = 0
+inicio_y = (filas - 1) * ancho_celda
+laberinto_turtle.penup()
+laberinto_turtle.goto(inicio_x, inicio_y)
+laberinto_turtle.pendown()
+
+# Dirección inicial de la tortuga
+direccion_actual = "up"
 
 # Función para dibujar una celda
 def dibujar_celda(x, y):
@@ -57,5 +66,65 @@ laberinto = [
 # Dibujar el laberinto
 dibujar_laberinto()
 
-# Cerrar la ventana al hacer clic
-ventana.exitonclick()
+laberinto_turtle.penup()
+laberinto_turtle.goto(30, 0)
+laberinto_turtle.pendown()
+
+
+
+# Asociar eventos de teclado a funciones
+def mover_adelante():
+    global direccion_actual
+    if direccion_actual == "up" and laberinto_turtle.ycor() < (filas - 1) * (ancho_celda/2):
+        laberinto_turtle.setheading(90)
+        laberinto_turtle.forward((ancho_celda/2))
+    elif direccion_actual == "down" and laberinto_turtle.ycor() > 0:
+        laberinto_turtle.setheading(270)
+        laberinto_turtle.forward((ancho_celda/2))
+    elif direccion_actual == "left" and laberinto_turtle.xcor() > 0:
+        laberinto_turtle.setheading(180)
+        laberinto_turtle.forward((ancho_celda/2))
+    elif direccion_actual == "right" and laberinto_turtle.xcor() < (columnas - 1) * (ancho_celda/2):
+        laberinto_turtle.setheading(0)
+        laberinto_turtle.forward((ancho_celda/2))
+
+def girar_izquierda():
+    global direccion_actual
+    if direccion_actual == "up":
+        direccion_actual = "left"
+        laberinto_turtle.setheading(180)
+    elif direccion_actual == "down":
+        direccion_actual = "right"
+        laberinto_turtle.setheading(0)
+    elif direccion_actual == "left":
+        direccion_actual = "down"
+        laberinto_turtle.setheading(270)
+    elif direccion_actual == "right":
+        direccion_actual = "up"
+        laberinto_turtle.setheading(90)
+
+def girar_derecha():
+    global direccion_actual
+    if direccion_actual == "up":
+        direccion_actual = "right"
+        laberinto_turtle.setheading(0)
+    elif direccion_actual == "down":
+        direccion_actual = "left"
+        laberinto_turtle.setheading(180)
+    elif direccion_actual == "left":
+        direccion_actual = "up"
+        laberinto_turtle.setheading(90)
+    elif direccion_actual == "right":
+        direccion_actual = "down"
+        laberinto_turtle.setheading(270)
+
+# Asociar eventos de teclado a funciones
+ventana.onkey(mover_adelante, "w")
+ventana.onkey(girar_izquierda, "a")
+ventana.onkey(girar_derecha, "d")
+
+# Escuchar eventos de teclado
+ventana.listen()
+
+# Mantener la ventana abierta
+ventana.mainloop()
