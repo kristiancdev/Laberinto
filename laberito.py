731506d14ab028d8afa4b6bf7fc234d3f1c235c6
Cristian Cevallos <81939696+kristiancdev@users.noreply.github.com>
2023-12-09 05:20:35 -0500

    Update laberito.py
    código refactorizado

--- laberito.py
+++ laberito.py
@@ -1,9 +1,9 @@
 import turtle
 
 # Definir el tamaño del laberinto
-ancho_celda = 20
-filas = 11
-columnas = 21
+ANCHO_CELDA = 20
+FILAS = 11
+COLUMNAS = 21
 
 # Crear una ventana de Turtle
 ventana = turtle.Screen()
@@ -15,41 +15,17 @@ laberinto_turtle = turtle.Turtle()
 laberinto_turtle.speed(0)  # Configurar la velocidad del dibujo
 
 # Posición inicial de la tortuga
-inicio_x = 0
-inicio_y = (filas - 1) * ancho_celda
+INICIO_X = 0
+INICIO_Y = (FILAS - 1) * ANCHO_CELDA
 laberinto_turtle.penup()
-laberinto_turtle.goto(inicio_x, inicio_y)
+laberinto_turtle.goto(INICIO_X, INICIO_Y)
 laberinto_turtle.pendown()
 
 # Dirección inicial de la tortuga
-direccion_actual = "up"
-
-# Función para dibujar una celda
-def dibujar_celda(x, y):
-    laberinto_turtle.color("black", "black")
-    laberinto_turtle.begin_fill()
-    laberinto_turtle.penup()
-    laberinto_turtle.goto(x, y)
-    laberinto_turtle.pendown()
-    laberinto_turtle.forward(ancho_celda)
-    laberinto_turtle.right(90)
-    laberinto_turtle.forward(ancho_celda)
-    laberinto_turtle.right(90)
-    laberinto_turtle.forward(ancho_celda)
-    laberinto_turtle.right(90)
-    laberinto_turtle.forward(ancho_celda)
-    laberinto_turtle.right(90)
-    laberinto_turtle.end_fill()
-
-# Función para dibujar el laberinto
-def dibujar_laberinto():
-    for fila in range(filas):
-        for columna in range(columnas):
-            if laberinto[fila][columna] == 1:
-                dibujar_celda(columna * ancho_celda, (fila * ancho_celda) * -1)
+DIRECCION_ACTUAL = "up"
 
 # Laberinto de ejemplo (1: pared, 0: pasillo)
-laberinto = [
+LABERINTO = [
     [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
@@ -63,63 +39,88 @@ laberinto = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
 ]
 
+# Dibujar una celda
+def dibujar_celda(x, y):
+    laberinto_turtle.color("black", "black")
+    laberinto_turtle.begin_fill()
+    laberinto_turtle.penup()
+    laberinto_turtle.goto(x, y)
+    laberinto_turtle.pendown()
+    for _ in range(4):
+        laberinto_turtle.forward(ANCHO_CELDA)
+        laberinto_turtle.right(90)
+    laberinto_turtle.end_fill()
+
 # Dibujar el laberinto
+def dibujar_laberinto():
+    for fila in range(FILAS):
+        for columna in range(COLUMNAS):
+            if LABERINTO[fila][columna] == 1:
+                dibujar_celda(columna * ANCHO_CELDA, (fila * ANCHO_CELDA) * -1)
+
+# Inicializar el laberinto
 dibujar_laberinto()
 
+# Posicionar la tortuga para empezar
 laberinto_turtle.penup()
 laberinto_turtle.goto(30, 0)
 laberinto_turtle.pendown()
 
+# Configurar la tortuga como cursor
+laberinto_turtle.shape("turtle")
+laberinto_turtle.color("red")
 
-# Asociar eventos de teclado a funciones
+# Mover la tortuga hacia adelante
 def mover_adelante():
-    global direccion_actual
+    global DIRECCION_ACTUAL
     siguiente_x, siguiente_y = laberinto_turtle.xcor(), laberinto_turtle.ycor()
 
-    if direccion_actual == "up":
-        siguiente_y += (ancho_celda / 2)
-    elif direccion_actual == "down":
-        siguiente_y -= (ancho_celda / 2)
-    elif direccion_actual == "left":
-        siguiente_x -= (ancho_celda / 2)
-    elif direccion_actual == "right":
-        siguiente_x += (ancho_celda / 2)
+    if DIRECCION_ACTUAL == "up":
+        siguiente_y += (ANCHO_CELDA / 2)
+    elif DIRECCION_ACTUAL == "down":
+        siguiente_y -= (ANCHO_CELDA / 2)
+    elif DIRECCION_ACTUAL == "left":
+        siguiente_x -= (ANCHO_CELDA / 2)
+    elif DIRECCION_ACTUAL == "right":
+        siguiente_x += (ANCHO_CELDA / 2)
 
     # Verificar si la próxima posición está dentro de un pasillo
-    fila = int(-siguiente_y / ancho_celda)
-    columna = int(siguiente_x / ancho_celda)
+    fila = int(-siguiente_y / ANCHO_CELDA)
+    columna = int(siguiente_x / ANCHO_CELDA)
 
-    if 0 <= fila < filas and 0 <= columna < columnas and laberinto[fila][columna] == 0:
+    if 0 <= fila < FILAS and 0 <= columna < COLUMNAS and LABERINTO[fila][columna] == 0:
         laberinto_turtle.goto(siguiente_x, siguiente_y)
 
+# Girar la tortuga a la izquierda
 def girar_izquierda():
-    global direccion_actual
-    if direccion_actual == "up":
-        direccion_actual = "left"
+    global DIRECCION_ACTUAL
+    if DIRECCION_ACTUAL == "up":
+        DIRECCION_ACTUAL = "left"
         laberinto_turtle.setheading(180)
-    elif direccion_actual == "down":
-        direccion_actual = "right"
+    elif DIRECCION_ACTUAL == "down":
+        DIRECCION_ACTUAL = "right"
         laberinto_turtle.setheading(0)
-    elif direccion_actual == "left":
-        direccion_actual = "down"
+    elif DIRECCION_ACTUAL == "left":
+        DIRECCION_ACTUAL = "down"
         laberinto_turtle.setheading(270)
-    elif direccion_actual == "right":
-        direccion_actual = "up"
+    elif DIRECCION_ACTUAL == "right":
+        DIRECCION_ACTUAL = "up"
         laberinto_turtle.setheading(90)
 
+# Girar la tortuga a la derecha
 def girar_derecha():
-    global direccion_actual
-    if direccion_actual == "up":
-        direccion_actual = "right"
+    global DIRECCION_ACTUAL
+    if DIRECCION_ACTUAL == "up":
+        DIRECCION_ACTUAL = "right"
         laberinto_turtle.setheading(0)
-    elif direccion_actual == "down":
-        direccion_actual = "left"
+    elif DIRECCION_ACTUAL == "down":
+        DIRECCION_ACTUAL = "left"
         laberinto_turtle.setheading(180)
-    elif direccion_actual == "left":
-        direccion_actual = "up"
+    elif DIRECCION_ACTUAL == "left":
+        DIRECCION_ACTUAL = "up"
         laberinto_turtle.setheading(90)
-    elif direccion_actual == "right":
-        direccion_actual = "down"
+    elif DIRECCION_ACTUAL == "right":
+        DIRECCION_ACTUAL = "down"
         laberinto_turtle.setheading(270)
 
 # Asociar eventos de teclado a funciones
